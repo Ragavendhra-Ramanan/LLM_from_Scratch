ba9c15d784ee404074329443cceb098eaff6bcb6
Ragavendhra Ramanan <ramananram1921@gmail.com>
2025-03-16 23:46:39 +0530

    download original gpt-2 model pretrained weights

--- gpt_model/pretrain.py
+++ gpt_model/pretrain.py
@@ -210,3 +210,16 @@ optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 
 #training
 model.train()
+
+import urllib.request
+url = ("https://raw.githubusercontent.com/rasbt/"
+       "LLMs-from-scratch/main/ch05/"
+       "01_main-chapter-code/gpt_download.py")
+filename = url.split('/')[-1]
+urllib.request.urlretrieve(url,filename)
+print("Filename",filename)
+from gpt_download import download_and_load_gpt2
+
+settings,params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
+print("Settings",settings)
+print("parameters",params.keys())
